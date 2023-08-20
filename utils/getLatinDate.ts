0cf2c4055fc609cd04d5592bee88d11e6a603fd6
coyotespike <timothy.roy@protonmail.ch>
2023-08-20 18:18:19 -0500

    Add macrons

--- utils/getLatinDate.ts
+++ utils/getLatinDate.ts
@@ -8,10 +8,9 @@ import {
 } from "./latinDateHelpers";
 
 export function getLatinDate(date: Date = new Date()): string {
-  const currentDate = new Date();
-  const day = currentDate.getDate();
-  const month = currentDate.getMonth() + 1; // Months are 0-indexed in JS
-  const year = currentDate.getFullYear();
+  const day = date.getDate();
+  const month = date.getMonth() + 1; // Months are 0-indexed in JS
+  const year = date.getFullYear();
 
   const ides = [3, 5, 7, 10].includes(month) ? 15 : 13;
   const nones = ides - 8;
@@ -19,32 +18,78 @@ export function getLatinDate(date: Date = new Date()): string {
   let latindate = "";
 
   if (day === 1 || day === nones || day === ides) {
-    // ... (This part remains unchanged)
+    switch (month) {
+      case 1:
+        latmon = "Iānuāriīs";
+        break;
+      case 2:
+        latmon = "Februāriīs";
+        break;
+      case 3:
+        latmon = "Mārtiīs";
+        break;
+      case 4:
+        latmon = "Aprīlibus";
+        break;
+      case 5:
+        latmon = "Māiīs";
+        break;
+      case 6:
+        latmon = "Iūniīs";
+        break;
+      case 7:
+        latmon = "Iūliīs";
+        break;
+      case 8:
+        latmon = "Augustīs";
+        break;
+      case 9:
+        latmon = "Septembribus"; // No macron needed
+        break;
+      case 10:
+        latmon = "Octōbribus";
+        break;
+      case 11:
+        latmon = "Novembribus"; // No macron needed
+        break;
+      case 12:
+        latmon = "Decembribus"; // No macron needed
+        break;
+      default:
+        return "ERROR: Invalid month.";
+    }
+  }
+  if (day === 1) {
+    latindate = `Hodiē est Kalendīs ${latmon} `;
+  } else if (day === nones) {
+    latindate = `Hodiē est Nōnīs ${latmon} `;
+  } else if (day === ides) {
+    latindate = `Hodiē est Idibus ${latmon} `;
   } else if (day >= 2 && day < nones) {
     latmon = getMonthNameForDaysBeforeNones(month);
     const num = nones - day + 1;
     const romanNum = num2roman(num).toLowerCase();
-    latindate = `Hodie est ante diem ${romanNum} Nonas ${latmon} `;
+    latindate = `Hodiē est ante diem ${romanNum} Nōnās ${latmon} `;
   } else if (day > nones && day < ides) {
     latmon = getMonthNameForDaysBeforeIdes(month);
     const num = ides - day + 1;
     const romanNum = num2roman(num).toLowerCase();
-    latindate = `Hodie est ante diem ${romanNum} Idus ${latmon} `;
+    latindate = `Hodiē est ante diem ${romanNum} Īdūs ${latmon} `;
   } else if (day === nones - 1) {
     latmon = getMonthNameForDaysBeforeNones(month);
-    latindate = `Hodie est pridie Nonas ${latmon} `;
+    latindate = `Hodiē est prīdiē Nōnās ${latmon} `;
   } else if (day === ides - 1) {
     latmon = getMonthNameForDaysBeforeIdes(month);
-    latindate = `Hodie est pridie Idus ${latmon} `;
+    latindate = `Hodiē est prīdiē Īdūs ${latmon} `;
   } else if (day > ides) {
     latmon = getMonthNameForDaysAfterIdes(month);
     const numDays = getNumDaysForMonth(month, year);
     const num = numDays - day + 1;
     const romanNum = num2roman(num).toLowerCase();
     if (romanNum === "ii") {
-      latindate = `Hodie est pridie Kalendas ${latmon} `;
+      latindate = `Hodiē est prīdiē Kalendās ${latmon} `;
     } else {
-      latindate = `Hodie est ante diem ${romanNum} Kalendas ${latmon} `;
+      latindate = `Hodiē est ante diem ${romanNum} Kalendās ${latmon} `;
     }
   } else {
     return "ERROR: day var didn't match any condition.";
--- utils/latinDate.test.ts
+++ utils/latinDate.test.ts
@@ -1,20 +1,32 @@
-import { getLatinDate } from "./getLatinDate"; // Adjust the import path if necessary
+import { getLatinDate } from "./getLatinDate";
 
 describe("getLatinDate", () => {
   it("should return correct Latin date for 1st January", () => {
     const result = getLatinDate(new Date("2023-01-01"));
-    expect(result).toBe("Hodie est Kalendis Ianuariis 2776 a.u.c.");
+    expect(result).toBe("Hodie est Kalendis Ianuariis MMDCCLXXVI a.u.c.");
   });
 
   it("should return correct Latin date for 15th March (Ides of March)", () => {
     const result = getLatinDate(new Date("2023-03-15"));
-    expect(result).toBe("Hodie est Idibus Martiis 2776 a.u.c.");
+    expect(result).toBe("Hodie est Idibus Martiis MMDCCLXXVI a.u.c.");
   });
 
   it("should handle leap years correctly", () => {
     const result = getLatinDate(new Date("2024-02-29"));
-    // Add expected result for 29th February in a leap year
+    expect(result).toBe(
+      "Hodie est ante diem II Kalendas Martias MMDCCLXXVII a.u.c."
+    );
   });
 
-  // Add more test cases as needed
+  it("should return correct Latin date for 5th March (Nones of March)", () => {
+    const result = getLatinDate(new Date("2023-03-05"));
+    expect(result).toBe("Hodie est Nonis Martiis MMDCCLXXVI a.u.c.");
+  });
+
+  it("should return correct Latin date for 13th April (ante diem III Idus Aprilis)", () => {
+    const result = getLatinDate(new Date("2023-04-13"));
+    expect(result).toBe(
+      "Hodie est ante diem III Idus Aprilis MMDCCLXXVI a.u.c."
+    );
+  });
 });
