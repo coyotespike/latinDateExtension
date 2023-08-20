1b6c94ff864d47f996941255c486e9c3b8d4434f
coyotespike <timothy.roy@protonmail.ch>
2023-08-20 17:22:51 -0500

    Add basic display to newtab

--- src/pages/newtab/Newtab.tsx
+++ src/pages/newtab/Newtab.tsx
@@ -1,23 +1,16 @@
-import React from 'react';
 import logo from '@assets/img/logo.svg';
 import '@pages/newtab/Newtab.css';
+import {getLatinDate} from '@utils/getLatinDate';
 
 export default function Newtab(): JSX.Element {
+  const today = getLatinDate();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/pages/newtab/Newtab.tsx</code> and save to reload.
+            {today}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
       </header>
     </div>
   );
