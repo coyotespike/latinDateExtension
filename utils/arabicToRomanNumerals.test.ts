c932bfda22d80655c0b611325c649994cd526a15
coyotespike <timothy.roy@protonmail.ch>
2023-08-20 21:45:13 -0500

    Replace helper with more readable implementation, add tests

--- utils/arabicToRomanNumerals.test.ts
+++ utils/arabicToRomanNumerals.test.ts
@@ -16,9 +16,21 @@ describe("arabicToRomanNumerals", () => {
     expect(arabicToRomanNumerals(1987)).toBe("MCMLXXXVII");
     expect(arabicToRomanNumerals(3999)).toBe("MMMCMXCIX");
   });
-
+  it("should handle years between 2020 and 2030", () => {
+    expect(arabicToRomanNumerals(2020)).toBe("MMXX");
+    expect(arabicToRomanNumerals(2021)).toBe("MMXXI");
+    expect(arabicToRomanNumerals(2022)).toBe("MMXXII");
+    expect(arabicToRomanNumerals(2023)).toBe("MMXXIII");
+    expect(arabicToRomanNumerals(2024)).toBe("MMXXIV");
+    expect(arabicToRomanNumerals(2025)).toBe("MMXXV");
+    expect(arabicToRomanNumerals(2026)).toBe("MMXXVI");
+    expect(arabicToRomanNumerals(2027)).toBe("MMXXVII");
+    expect(arabicToRomanNumerals(2028)).toBe("MMXXVIII");
+    expect(arabicToRomanNumerals(2029)).toBe("MMXXIX");
+    expect(arabicToRomanNumerals(2030)).toBe("MMXXX");
+  });
   it("should handle zero input", () => {
-    expect(arabicToRomanNumerals(0)).toBe("");
+    expect(() => arabicToRomanNumerals(0)).toThrow();
   });
 
   it("should handle large input numbers", () => {
--- utils/arabicToRomanNumerals.ts
+++ utils/arabicToRomanNumerals.ts
@@ -1,76 +1,34 @@
 function arabicToRomanNumerals(input: number): string {
-  let output = "";
-  const numStr = input.toString();
-  const len = numStr.length;
-
-  function roman_val(
-    N: number,
-    one: string,
-    five: string,
-    ten: string
-  ): string {
-    let out = "";
-
-    switch (N) {
-      case 0:
-        break;
-      case 1:
-      case 2:
-      case 3:
-        while (N > 0) {
-          out += one;
-          N--;
-        }
-        break;
-      case 4:
-        out += one + five;
-        break;
-      case 5:
-        out += five;
-        break;
-      case 6:
-      case 7:
-      case 8:
-        out += five;
-        N -= 5;
-        while (N > 0) {
-          out += one;
-          N--;
-        }
-        break;
-      case 9:
-        out += one + ten;
-        break;
-    }
-    return out;
+  if (input <= 0) {
+    throw new Error("Input must be a positive integer");
   }
 
-  for (let i = 0; i < len; i++) {
-    const num = parseInt(numStr[i]);
+  const romanNumerals: [number, string][] = [
+    [1000, "M"],
+    [900, "CM"],
+    [500, "D"],
+    [400, "CD"],
+    [100, "C"],
+    [90, "XC"],
+    [50, "L"],
+    [40, "XL"],
+    [10, "X"],
+    [9, "IX"],
+    [5, "V"],
+    [4, "IV"],
+    [1, "I"],
+  ];
 
-    switch (len - i) {
-      case 1:
-        output += roman_val(num, "I", "V", "X");
-        break;
-      case 2:
-        output += roman_val(num, "X", "L", "C");
-        break;
-      case 3:
-        output += roman_val(num, "C", "D", "M");
-        break;
-      case 4:
-        output += roman_val(num, "M", "V", "X");
-        break;
-      default:
-        while (input > 0) {
-          output += "M";
-          input--;
-        }
-        break;
+  let result = "";
+
+  for (const [num, numeral] of romanNumerals) {
+    while (input >= num) {
+      result += numeral;
+      input -= num;
     }
   }
 
-  return output;
+  return result;
 }
 
 export default arabicToRomanNumerals;
